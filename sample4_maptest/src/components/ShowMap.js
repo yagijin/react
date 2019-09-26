e1463ec3d70063c0c401ee31bc010582f1f40370
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-09-26 17:15:00 +0900

    Add：静岡県の表示

--- sample4_maptest/src/components/ShowMap.js
+++ sample4_maptest/src/components/ShowMap.js
@@ -1,29 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState} from 'react';
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
+
 import './Map.css';
 import token from '../key.js'
 
 export default function ShowMap () {
-  mapboxgl.accessToken = token();
-  var map;
-  var container;
+  
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
 
   useEffect(() => {
+    mapboxgl.accessToken = token();
     //地図の描画の初期化
-    map = new mapboxgl.Map({
-      container: container,
-      style: 'mapbox://styles/mapbox/streets-v9',
-    })
-    //地図をクリーンアップ
-    return () => {
-      map.remove()
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [138.23, 34.58],
+        bounds: [[137.4000, 34.4000],[139.2000, 35.8000]],
+        zoom: 5
+      });
+
+      map.on("load", () => {
+        setMap(map);
+        map.resize();
+      });
     };
+
+    if (!map) initializeMap({ setMap, mapContainer });
+
     //第２引数として空の配列[]を渡すとこの副作用がコンポーネントの
     //どの値にも依存してないことがReact側に伝えられて更新時には実行されなくなる
-  },[]);
+  }, [map]);
 
   return (
-      <div className={'map'} ref={e => (container = e)} />
+      <div ref={el => (mapContainer.current = el)} className={'map'} />
   );
 }
