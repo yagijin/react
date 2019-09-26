7a2b34a2d198711701fe8ea9e81c8295edb262b7
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-09-26 12:31:46 +0900

    Add:MapboxGLを使えるように設定

--- sample4_maptest/src/App.js
+++ sample4_maptest/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import './App.css';
 
-function App() {
+export default function App() {
+  var map;
+  var container;
+  
+  useEffect(() => {
+    map = new mapboxgl.Map({
+      container: container,
+      style: 'mapbox://styles/mapbox/streets-v9',
+    })
+    return () => {
+      map.remove()
+    };
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={'map'} ref={e => (container = e)} />
   );
 }
-
-export default App;
--- sample4_maptest/src/index.js
+++ sample4_maptest/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import mapboxgl from 'mapbox-gl'
+import token from './key.js'
+
+mapboxgl.accessToken = token();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
