5062c0093ec22b6da1a4cafe31f15a1a852788c2
jinyagi <jinjin.0327.jinjin@gmail.com>
2019-10-24 11:16:33 +0900

    INIT

---
+++ sample9_react-scripts/src/App.js
@@ -0,0 +1,15 @@
+import React , { useState }from 'react';
+import './App.css';
+
+export default function App() {
+
+    const [content, setContent] = useState({});
+  
+    return (
+      <div>
+        <header className="App">
+          <a>Hey</a>
+        </header>
+      </div>
+    );
+  }
